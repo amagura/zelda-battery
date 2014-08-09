8d8ca66a6360aca3e328128d497434f76889d9a8
dopm <alexej@ifit.com>
2014-08-09 15:42:46 -0600

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 var gulp    = require('gulp')
   , concat  = require('gulp-concat')
   ;
+
+gulp.task('default', [ 'build' ]);
+
+gulp.task('build', function() {
+  gulp.src('./src/*.jl')
+    .pipe(concat('zbat.jl'))
+    .pipe(gulp.dest('./'))
+});
