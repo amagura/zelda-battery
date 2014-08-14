63e2d0c8af185d9e820f36524e962b01df377d98
dopm <alexej@ifit.com>
2014-08-14 14:41:31 -0600

    that helped speed things up a little bit, still a little slower

--- zbb.py
+++ zbb.py
@@ -21,18 +21,18 @@
   along with Zelda Battery.  If not, see <http://www.gnu.org/licenses/>.
 """
 from subprocess import getoutput as run;
-import re
-import sys
+from re import search, match;
+from sys import platform;
 
 _acBlink = False
 
 def onAcPower():
-  if sys.platform.startswith('linux'):
+  if platform.startswith('linux'):
     return run('acpi -a').find('on-line') is not -1
-  elif sys.platform.startswith('freebsd'):
-    return re.search('AC', re.match('.*on-line.*', run('apm')).group()) is not None
-  elif sys.platform.startswith('darwin'):
-    return re.search('Power', re.match('.*\*.*', re.match('.*AC.*', run('pmset -g ps')))) is not None
+  elif platform.startswith('freebsd'):
+    return search('AC', match('.*on-line.*', run('apm')).group()) is not None
+  elif platform.startswith('darwin'):
+    return search('Power', match('.*\*.*', match('.*AC.*', run('pmset -g ps')))) is not None
 
 def perc_tInt(perc):
   perc = float(perc)
@@ -42,8 +42,8 @@ def perc_tInt(perc):
   return perc
 
 def powerLvl():
-  if sys.platform.startswith('linux'):
-    return re.search('([0-9]+)%', run('acpi')).group(1)
+  if platform.startswith('linux'):
+    return search('([0-9]+)%', run('acpi')).group(1)
   #elif sys.platform.startswith('freebsd'):
 
 def heart(boo, n):
