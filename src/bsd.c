921f142b605b837c7f159f9387dd710ac84fc9cc
dopm <alexej@ifit.com>
2014-12-25 01:47:11 -0700

    stuff

--- src/bsd.c
+++ src/bsd.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <malloc.h>
 #include <unistd.h>
-#include <sysctl.h> /* needs -lc */
+#include <sys/sysctl.h> /* needs -lc */
 
 #if _ZB_MAKING_ZB_COLOR
 #define _ZB_PROGNAME "zbac"
@@ -14,15 +13,33 @@
 inline bool
 on_ac_pwr()
 {
-  int size;
+  size_t size;
   int ac_line;
   size = sizeof(int);
   sysctlbyname("hw.acpi.acline", &ac_line, &size, NULL, false);
   return (bool)ac_line;
 }
 
+inline int
+charge()
+{
+  size_t size;
+  int batt_charge;
+  size = sizeof(int);
+  sysctlbyname("hw.acpi.battery.life", &batt_charge, &size, NULL, false);
+  return (int)(batt_charge / 10);
+}
+
 int
 main()
 {
-  printf("%d\n", ac_line);
+#
+  struct power_t {
+    int charge;
+    bool ac_line;
+  } power;
+  power.charge = charge();
+  power.ac_line = on_ac_pwr();
+
+  for (int hdx = 1; hdx <= 
 }
