7ba24945c4ef66f279868a658fadffc660dd07e6
Alexej Magura <sickhadas.code@gmail.com>
2015-02-18 21:58:09 -0700

    rewriting a lot of code

--- src/power.c
+++ src/power.c
@@ -32,37 +32,42 @@ limitations under the License.
 
 void getpwr(struct power *pwr)
 {
-     int *limit;
-     if ((limit = &pwr->charge.nof) < 0)
-	  *limit = 1;
+     int *btnum;
+     if ((btnum = &pwr->charge.nof) < 0)
+	  *btnum = 1;
+     pwr->e = pwr->err;
 
 #if ZB_LINUX
      struct pwr_sup info;
+     info.e = pwr->e;
      info.cap = -1;
 
      /* check if there was an error */
-     if ((pwr->err = pwr_info(&info, *limit)) != PWR_OK) {
-	  ZB_DBG("err: %d\n", pwr->err);
+     pwr_info(&info, *btnum);
+     if (*info.e != PWR_OK) {
+	  ZB_DBG("err: %d\n", *info.e);
 	  ZB_ONDBG(perror(ZB_PROGNAME));
      }
-
 #elif ZB_BSD
      size_t size;
      int ac_line;
      int limit = pwr->charge.nof;
-     pwr->charge.
-
+     pwr->e = pwr->err;
      size = sizeof(int);
      /* determine if we're running on battery or ac power */
      ZB_DBG("%s\n", "getting hw.acpi.acline");			\
-     if (sysctlbyname("hw.acpi.acline", &ac_line, &size, NULL, false) == -1)
+     if (sysctlbyname("hw.acpi.acline", &ac_line, &size, NULL, false) == -1) {
 	  ZB_ONDBG(perror(ZB_PROGNAME));
-     pwr->acline = (bool)ac_line;
+     } else {
+	  pwr->acline = (bool)ac_line;
+     }
 
      /* determine how much battery power is left */
      ZB_DBG("%s\n", "getting hw.acpi.battery.life");			\
-     sysctlbyname("hw.acpi.battery.life", &ac_line, &size, NULL, false);
-
+     if (sysctlbyname("hw.acpi.battery.life", &ac_line, &size, NULL, false) == -1) {
+	  ZB_ONDBG(perror(ZB_PROGNAME));
+     } else {
+     }
      pwr->charge.raw = (limit != 0)
 	  ? ac_line
 	  : ZB_PWR_NWANTBAT;
