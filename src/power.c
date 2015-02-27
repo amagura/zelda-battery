257a10c319adedebacdeaaed1e1ef4979e48f659
Alexej Magura <sickhadas.code@gmail.com>
2015-02-27 02:16:23 -0700

    hope this works

--- src/power.c
+++ src/power.c
@@ -61,6 +61,7 @@ void getpwr(struct power *pwr)
      size = sizeof(int);
      /* determine if we're running on battery or ac power */
      ZB_DBG("%s\n", "getting hw.acpi.acline");
+# ifndef HAVE_LIBC /* means no `sysctlbyname`; not that we don't have libc */
      if (sysctlbyname("hw.acpi.acline", &ac_line, &size, NULL, false) == -1) {
 	  ZB_ONDBG(perror(ZB_PROGNAME));
      } else {
