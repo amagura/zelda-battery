c1f0f489d3958279b7c30c002489c02ac1ad31fa
Alexej Magura <sickhadas.code@gmail.com>
2015-02-26 21:09:16 -0700

    no more alloca

--- src/compat.h
+++ src/compat.h
@@ -31,33 +31,6 @@ limitations under the License.
 
 /** ^^ Macros ^^ **/
 
-/* Alloca crap */
-# ifdef STDC_HEADERS
-#  include <stdlib.h>
-#  include <stddef.h>
-# else
-#  ifdef HAVE_STDLIB_H
-#   include <stdlib.h>
-#  endif
-# endif
-# ifdef HAVE_ALLOCA_H
-#  include <alloca.h>
-# elif !defined alloca
-#  ifdef __GNUC__
-#   define alloca __builtin_alloca
-#  elif defined _AIX
-#   define alloca __alloca
-#  elif defined _MSC_VER
-#   include <malloc.h>
-#   define alloca _alloca
-#  elif !defined HAVE_ALLOCA
-#   ifdef  __cplusplus
-extern "C"
-#   endif
-void *alloca (size_t);
-#  endif
-# endif
-
 /*** ^^ Compiler Compat macros ^^ ***/
 # if !__clang__
 #  ifndef __has_attribute
@@ -72,6 +45,7 @@ void *alloca (size_t);
 #  endif
 # endif
 
+/* inline functions really aren't what we think they are */
 # define inline
 
 # if __GNUC__ && !__clang__
