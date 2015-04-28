0e9cd51fc1372ad3e839220be2b7e02d74f5155d
Alexej Magura <sickhadas.code@gmail.com>
2015-04-28 17:57:45 -0600

    yaaaY!!! it works!!

--- win/power.cxx
+++ win/power.cxx
@@ -1,1 +1,12 @@
-#include <Winbase.h>
+
+#define _WIN32_WINNT NTDDI_WINXP
+#include <Windows.h>
+
+
+int main()
+{
+     SYSTEM_POWER_STATUS syspwr;
+     if (GetSystemPowerStatus(&syspwr))
+	  std::cout << "AC line active: " << syspwr.ACLineStatus == 1 ? "yes" : "no" << std::endl;
+     return 0;
+}
