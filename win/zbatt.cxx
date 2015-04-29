94a198a7f29b8c40820f61b24bd448ae3c8d4ca0
Alexej Magura <sickhadas.code@gmail.com>
2015-04-29 12:55:33 -0600

    ..

---
+++ win/zbatt.cxx
@@ -0,0 +1,31 @@
+/****
+Copyright 2015 Alexej Magura
+
+This file is a part of ZBatt
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+****/
+#include "main.hxx"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+     int optc = argc;
+     char **optv = argv;
+
+     optc -= (optc > 0);
+     optv += (optc > 0);
+
+
+     return 0;
+}
