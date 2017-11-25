c9dc6d61a8dbecb336c7927c9a0d156f7aa47f32
Alexej G. Magura <sickhadas.code@gmail.com>
2017-11-24 20:32:53 -0700

    refactoring most of configure.ac

--- src/power.c
+++ src/power.c
@@ -26,7 +26,7 @@ limitations under the License.
 
 #if ZB_LINUX
 # include "acpi.h"
-#elif ZB_BSD
+#elif HAVE__SYSCTLBYNAME
 // XXX this fixes a compiler error on FreeBSD 10.1-RELEASE-p6
 typedef unsigned int u_int;
 # include <sys/sysctl.h>
